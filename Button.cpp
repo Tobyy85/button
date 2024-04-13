77a20f40b8b169c2afa096ec37fbbd7ae6620eb1
Tobias <tobias.ninjas@gmail.com>
2024-04-13 15:44:12 +0200

    Reworked

--- Button.cpp
+++ Button.cpp
@@ -1,11 +1,9 @@
-//TODO - add option to wait before showing single click if it's not a double or triple click
-//TODO Implemnt ^ for press and hold as well
-
 #include "Button.h"
 
-Button::Button(int pin, bool input_pullup){
+Button::Button(int pin, bool input_pullup, int multiple_click_time){
   this->pin = pin;
   this->input_pullup = input_pullup;
+  this->multiple_click_time = multiple_click_time;
   if (input_pullup){
     pinMode(pin, INPUT_PULLUP);
   }else{
@@ -13,136 +11,40 @@ Button::Button(int pin, bool input_pullup){
   }
 }
 
-void Button::update(){
+
+bool Button::get_state(){
   if (this->input_pullup){
-    this->state = (digitalRead(this->pin) == LOW);
+    return digitalRead(this->pin) == LOW;
   }else{
-    this->state = (digitalRead(this->pin) == HIGH);
+    return digitalRead(this->pin) == HIGH;
   }
-    
-  if (this->state && !this->last_state){
-    this->clicks_time[this->clicks_index][0] = millis();
-    this->position = "UP";
-  }
-  if (!this->state && this->last_state){
-    this->clicks_time[this->clicks_index][1] = millis();
-    this->position = "DOWN";
-    this->returned = false;
-    if (this->clicks_index < this->max_values-1){
-      this->clicks_index++;
+}
+
+bool Button::pressed_down(){
+  return this->state && !this->last_state;
+}
+
+void Button::update(){
+  this->state = this->get_state();
+  if (pressed_down()){
+    this->last_click_millis = this->click_millis;
+    this->click_millis = millis();
+    if (this->click_millis - this->last_click_millis <= this->multiple_click_time){
+      this->count++;
     }else{
-      this->clicks_index = 0;
+      this->count = 1;
     }
+    this->changed = true;
   }
-  this->last_state = state;
+  this->last_state = this->state;
 }
 
-long** Button::get_sorted_values(unsigned long time_arr[][2], int current_index){
-  long** new_arr = new long*[this->max_values];
-  for (int i = 0; i < this->max_values; i++){
-    new_arr[i] = new long[2];
-  }
-  
-  for (int i = current_index-1, j = 0; i > current_index - this->max_values-1; i--, j++){
-    int index = i;
-    if (i < 0){
-      index = this->max_values + i;
-    }
-    new_arr[j][0] = time_arr[index][0];
-    new_arr[j][1] = time_arr[index][1];
-  }
-  return new_arr;
-}
-
-bool Button::is_long_click(long** sorted_values){
-  return sorted_values[0][1] - sorted_values[0][0] >= this->long_click;
-}
-
-bool Button::is_single_click(long** sorted_values){
-  int difference = sorted_values[0][1] - sorted_values[0][0];
-  return 0 < difference && difference < this->long_click;
-}
-
-bool Button::is_double_click(long** sorted_values){
-  return sorted_values[0][0] - sorted_values[1][1] < this->multiple_click;
-}
-
-bool Button::is_triple_click(long** sorted_values){
-  return sorted_values[0][0] - sorted_values[1][1] < this->multiple_click && sorted_values[1][0] - sorted_values[2][1] < this->multiple_click;
-}
-
-bool Button::is_press_and_hold(long** sorted_values){
-  int difference = sorted_values[1][1] - sorted_values[1][0];
-  bool is_single = 0 < difference && difference < this->long_click;
-  bool is_double = sorted_values[0][0] - sorted_values[1][1] < this->multiple_click;
-  bool is_long = sorted_values[0][1] - sorted_values[0][0] >= this->long_click;
-  return is_single && is_double && is_long; 
-}
-
-String Button::get(bool wait_for_multiple_click){
-  update();
-  long** sorted_values = get_sorted_values(this->clicks_time, this->clicks_index);
-  String click_type = "NO CLICK";
-
-  if (is_press_and_hold(sorted_values) && !this->returned){ //press and hold
-    this->returned = true;
-    click_type = "PRESS AND HOLD";
-  }else if (is_long_click(sorted_values) && !this->returned){ //long click
-      this->returned = true;
-      click_type = "LONG CLICK";
+int Button::get(){
+  unsigned long current_millis = millis();
+  if (current_millis - this->click_millis > this->multiple_click_time && this->changed){
+    this->changed = false;
+    return this->count;
   }else{
-    if (wait_for_multiple_click){
-      if (is_triple_click(sorted_values) && !this->returned){ //triple click
-        this->returned = true;
-        this->return_double = true;
-        click_type = "TRIPLE CLICK";
-      }else if (is_double_click(sorted_values)){ //double click
-        if (millis() -  sorted_values[0][0] >= this->multiple_click && this->return_single){
-          if ( !this->returned){
-            this->returned = true;
-            this->return_double = false;
-            click_type = "DOUBLE CLICK";
-          }
-        }else{
-          this->return_double = true;
-        }
-      }else if (is_single_click(sorted_values)){ //single click
-        if (millis() - sorted_values[0][0] >= this->multiple_click && this->return_single){
-          if (!this->returned){
-            this->returned = true;
-            click_type = "SINGLE CLICK";
-          }
-        }else{
-          this->return_single = true;
-        }
-      }
-    }else{
-      if (is_triple_click(sorted_values) && !this->returned){ //triple click
-        this->returned = true;
-        click_type = "TRIPLE CLICK";
-      }else if (is_double_click(sorted_values) && !this->returned){ //double click
-        this->returned = true;
-        click_type = "DOUBLE CLICK";
-      }else if (is_single_click(sorted_values) && !this->returned){ //single click
-        this->returned = true;
-        click_type = "SINGLE CLICK";
-      }
-    }
+    return 0;
   }
-
-
-  for (int i = 0; i < max_values; i++){
-    delete[] sorted_values[i];
-  }
-  delete[] sorted_values;
-  return click_type;
-}
-
-bool Button::is_pressed(){
-  update();
-  return this->state;
-}
-
-String Button::get_position(){
-  return this->position;
 }
--- Button.h
+++ Button.h
@@ -5,34 +5,24 @@
 
 class Button{
     public:
-        Button(int pin, bool input_pullup = true);
-        String get(bool wait_for_multiple_click = true);
-        bool is_pressed();
-        String get_position();
+        Button(int pin, bool input_pullup = true, int multiple_click_time = 400); // int multiple_click_time = 300
+        bool get_state();
+        void update();
+        int get();
 
     private:
-        void update();
-        long** get_sorted_values(unsigned long time_arr[][2], int current_index);
-        bool is_press_and_hold(long** sorted_values);
-        bool is_long_click(long** sorted_values);
-        bool is_single_click(long** sorted_values);
-        bool is_double_click(long** sorted_values);
-        bool is_triple_click(long** sorted_values);
-
+        bool pressed_down();
         int pin;
         bool input_pullup;
-        int state;
-        int last_state;
-        int clicks_index = 0;
-        String position = "UP";
-        int long_click = 400;
-        int multiple_click = 300;
-        bool returned = true;
-        bool return_single = true;
-        bool return_double = true;
-        
-        static const int max_values = 3;
-        unsigned long clicks_time[max_values][2] = {};
+        int multiple_click_time;
+
+        bool last_state = false;
+        bool state = false;
+        int count = 0;
+        unsigned long last_click_millis = 0;
+        unsigned long click_millis = 0;
+        bool changed = false;
+
 };
 
 #endif

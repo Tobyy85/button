2e38db2fd8c3612dba3bbccea182fe28233024d3
Tobias <tobias.ninjas@gmail.com>
2024-04-13 16:25:08 +0200

    Refactor logic of multiple click detection

--- Button.cpp
+++ Button.cpp
@@ -1,8 +1,9 @@
 #include "Button.h"
 
-Button::Button(int pin, bool input_pullup, int multiple_click_time){
+Button::Button(int pin, bool input_pullup, int long_click_time, int multiple_click_time){
   this->pin = pin;
   this->input_pullup = input_pullup;
+  this->long_click_time = long_click_time;
   this->multiple_click_time = multiple_click_time;
   if (input_pullup){
     pinMode(pin, INPUT_PULLUP);
@@ -24,24 +25,34 @@ bool Button::pressed_down(){
   return this->state && !this->last_state;
 }
 
-void Button::update(){
-  this->state = this->get_state();
-  if (pressed_down()){
-    this->last_click_millis = this->click_millis;
-    this->click_millis = millis();
-    if (this->click_millis - this->last_click_millis <= this->multiple_click_time){
-      this->count++;
-    }else{
-      this->count = 1;
-    }
-    this->changed = true;
-  }
-  this->last_state = this->state;
+bool Button::pressed_up(){
+  return !this->state && this->last_state;
 }
 
-int Button::get(){
-  unsigned long current_millis = millis();
-  if (current_millis - this->click_millis > this->multiple_click_time && this->changed){
+void Button::update(){
+    this->last_state = this->state;
+    this->state = get_state();
+
+    if (pressed_down()){
+      this->last_click_down_millis = this->click_down_millis;
+      this->click_down_millis = millis();
+        if (click_down_millis - last_click_up_millis <= this->multiple_click_time){
+          this->count++;
+        }
+        else{
+          this->count = 1;
+        }
+      this->changed = true;
+    }
+
+    if (pressed_up()){
+      this->last_click_up_millis = this->click_up_millis;
+      this->click_up_millis = millis();
+    }
+}
+
+int Button::get(){  
+  if (millis() - this->click_down_millis > this->multiple_click_time && this->changed){
     this->changed = false;
     return this->count;
   }else{

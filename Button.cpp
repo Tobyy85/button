289019742897b2727fd2cb172195c9b87af01b2d
Tobias <tobias.ninjas@gmail.com>
2024-04-13 16:33:57 +0200

    Fixed bugs

--- Button.cpp
+++ Button.cpp
@@ -1,9 +1,8 @@
 #include "Button.h"
 
-Button::Button(int pin, bool input_pullup, int long_click_time, int multiple_click_time){
+Button::Button(int pin, bool input_pullup, int multiple_click_time){
   this->pin = pin;
   this->input_pullup = input_pullup;
-  this->long_click_time = long_click_time;
   this->multiple_click_time = multiple_click_time;
   if (input_pullup){
     pinMode(pin, INPUT_PULLUP);
@@ -25,37 +24,26 @@ bool Button::pressed_down(){
   return this->state && !this->last_state;
 }
 
-bool Button::pressed_up(){
-  return !this->state && this->last_state;
-}
-
 void Button::update(){
-    this->last_state = this->state;
-    this->state = this->get_state();
-
-    if (this->pressed_down()){
-      this->last_click_down_millis = this->click_down_millis;
-      this->click_down_millis = millis();
-        if (click_down_millis - last_click_up_millis <= this->multiple_click_time){
-          this->count++;
-        }
-        else{
-          this->count = 1;
-        }
-      this->changed = true;
+  this->state = this->get_state();
+  if (pressed_down()){
+    this->last_click_millis = this->click_millis;
+    this->click_millis = millis();
+    if (this->click_millis - this->last_click_millis <= this->multiple_click_time){
+      this->count++;
+    }else{
+      this->count = 1;
     }
-
-    if (this->pressed_up()){
-      this->last_click_up_millis = this->click_up_millis;
-      this->click_up_millis = millis();
-    }
+    this->changed = true;
+  }
+  this->last_state = this->state;
 }
 
-int Button::get(bool update){  
+int Button::get(bool update){
   if (update){
     this->update();
   }
-  if (millis() - this->click_down_millis > this->multiple_click_time && this->changed){
+  if (millis() - this->click_millis > this->multiple_click_time && this->changed){
     this->changed = false;
     return this->count;
   }else{
--- Button.h
+++ Button.h
@@ -5,27 +5,22 @@
 
 class Button{
     public:
-        Button(int pin, bool input_pullup = true, int long_click_time = 400, int multiple_click_time = 400); // int multiple_click_time = 300
+        Button(int pin, bool input_pullup = true, int multiple_click_time = 400); // int multiple_click_time = 300
         bool get_state();
         void update();
         int get(bool update = true);
 
     private:
         bool pressed_down();
-        bool pressed_up();
         int pin;
         bool input_pullup;
-        int long_click_time;
         int multiple_click_time;
-
 
         bool last_state = false;
         bool state = false;
         int count = 0;
-        unsigned long click_down_millis = 0;
-        unsigned long click_up_millis = 0;
-        unsigned long last_click_down_millis = 0;
-        unsigned long last_click_up_millis = 0;
+        unsigned long last_click_millis = 0;
+        unsigned long click_millis = 0;
         bool changed = false;
 
 };
